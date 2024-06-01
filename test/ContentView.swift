9e0d0438d6a984495ff41cbb6e3752487b9cbdb5
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-06-01 18:27:15 -0500

    Update: navigation/practice
    Just playing around w/ swift, got this small nav screen working. (this 2017 macbook is struggling lol)

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,13 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Screen(
+                title: "Social Coding",
+                text: "we love momo!",
+                imageName: "house"
+            )
         }
-        .padding()
+    }
+}
+
+struct Screen: View {
+    
+    let title: String
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200, alignment: .center)
+                .padding()
+            
+            Text(text)
+                .font(.system(size: 30, weight: .light, design: .default))
+                .padding()
+            
+            NavigationLink(
+                destination: Screen(
+                    title: "About Us",
+                    text: "What is MOBI?",
+                    imageName: "globe"
+                ),
+                label: {
+                    ContinueButton(color: .mint)
+                }
+            )
+        }
+        .navigationTitle(title)
+
+    }
+}
+
+struct ContinueButton: View {
+    
+    let color: Color
+    
+    var body: some View {
+        Text("Continue")
+            .frame(width: 200, height: 50, alignment: .center)
+            .background(color)
+            .foregroundColor(.white)
+            .cornerRadius(8)
     }
 }
 
