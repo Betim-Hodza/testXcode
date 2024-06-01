f9990728365bb62659a9dc8af658adf4ed1cbd06
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-06-01 13:01:31 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Betim Hodza on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
